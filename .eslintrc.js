de0bce89d925409559ed1229f7097c606f3634ec
duecoder <eduardodecastro@outlook.com>
2022-09-23 10:17:17 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:testing-library/react',
+    'plugin:prettier/recommended',
+    'plugin:jest/all',
+    'standard',
+    'airbnb',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/state-in-constructor': 'off',
+    'react/forbid-prop-types': 0,
+    'jest/require-hook': 'off',
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>'Hello World'</p>
+        <Main />
+      </header>
+    </div>
+  );
+}
